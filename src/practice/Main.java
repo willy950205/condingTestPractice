50a79118e539a816f66cd39f76463c61d7906b99
willy950205 <willy950205@gmail.com>
2021-01-12 18:06:37 +0900

    ProThievery Commit

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -2,9 +2,15 @@ package practice;
 
 public class Main {
     public static void main(String [] args){
-        ProUnfinishedMan p = new ProUnfinishedMan();
-        String [] participant = {"mislav","mislav","stanko", "ana"};
-        String [] completion = {"mislav","stanko", "ana"};
-        p.solution(participant,completion);
+        ProThievery a = new ProThievery();
+        //int [] money = {1,3,7,9,5,1,7,1,8};
+        //int [] money = {7,9,5,1,7,1,8,1,3};
+        int [] money = {1,2,3,1};
+        //int [] money = {1,0,0,0,0,0,0,0,0,0,0,0,0,90};
+
+        int answer = a.solution(money);
+
+        System.out.println(answer);
+
     }
 }
