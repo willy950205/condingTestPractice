3ba911aa2180f2b7e9483399f58ec7a0a040be5f
unknown <willy950205@gmail.com>
2021-01-27 18:00:26 +0900

    210127 study

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -1,26 +1,13 @@
 package programmers;
 
-import java.util.PriorityQueue;
-
 public class Main {
     public static void main(String [] args){
         
-//    	ProRanking a = new ProRanking();
-//    	int results [][] = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
-//    	a.solution(5, results);
+    	Immigration a = new Immigration();
     	
-    	PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+    	int times [] = {7,10};
     	
-    	pq.add(10);
-    	pq.add(5);
-    	pq.add(100);
-    	pq.add(1);
-    	pq.add(13);
-    	
-     
-    	while(!pq.isEmpty()) {
-    		System.out.println(pq.poll());
-    	}
+    	System.out.println(a.solution(6, times));
     	
     }
 }
